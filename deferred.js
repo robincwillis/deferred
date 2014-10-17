6d62cada60c6e208e06953a51de3966da50f2019
robincwillis <robincwillis@gmail.com>
2014-10-17 02:17:00 -0400

    trimmed down even more

--- deferred.js
+++ deferred.js
@@ -11,50 +11,18 @@ function Deferred(fn) {
 	var promise = {
 
 		done: function(){
-			console.log('done called');
 			for(var i = 0; i < arguments.length; i++){
-				if(isArray(arguments[i])){
-					var arr = arguments[i];
-					for(var j = 0; j < arr.length; j++){
-						if(status === 'rejected'){
-							console.log('rejected');
-							arr[j].apply(this, resultArgs);
-						}
-						failsFuncs.push(arr[j]);
-					}
-
-				}else{
-					if(status === 'resolved'){
-						console.log('rejected');
-						arguments[i].appy(this, resultArgs);
-					}
 					doneFuncs.push(arguments[i]);
-				}
 			}
 			return this;
 		},
 		fail: function(){
-			console.log('fail called');
-			for (var i = 0; i < arguments.length; i++){
-				if(isArray(arguments[i])){
-					var arr = arguments[i];
-					for (var j = 0; j < arr.length; j++) {
-						if (status === 'rejected') {
-							arr[j].apply(this, resultArgs);
-						}
-						failFuncs.push(arr[j]);
-					}
-				}else{
-					if(status === 'rejected') {
-						arguments[i].apply(this, resultsArgs);
-					}
+			 for (var i = 0; i < arguments.length; i++){
 					failFuncs.push(arguments[i]);
-				}
 			}
 			return this;
 		},
 		then : function(){
-
 			if (arguments.length > 1 && arguments[1]){
 				this.fail(arguments[1]);
 			}
@@ -90,18 +58,16 @@ function Deferred(fn) {
 				status = 'rejected';
 
 				var args = (arguments.length > 1) ? arguments[1] : [];
-				for(var i = 0; i < failFuncs.length; i++){
+				for(var i = 0; i < failFuncs.length; i++){;
 					failFuncs[i].apply(context, args);
 				}
 			}
 			return this;
 		},
 		resolve : function(){
-			console.log('resolve called now');
 			return this.resolveWith(this, arguments);
 		},
 		reject : function(){
-			console.log('reject called now');
 			return this.rejectWith(this, arguments);
 		},
 	}
@@ -120,21 +86,21 @@ Deferred.when = function(){
 	return (function(args){
 		var dfd = Deferred();
 		var done = 0;
-		var rp = new Array(args.length);
+		var resp = new Array(args.length);
 
 		for(var i=0; i < args.length; i++){
 			(function(j){
 				args[j]
 				.done(function(){
-					rp[j] = (arguments.length < args.length) ? arguments[0] : arguments;
+					resp[j] = (arguments.length < args.length) ? arguments[0] : arguments;
 					if(++ done == args.length){
-						dfd.resolve.apply(dfd, rp);
+						dfd.resolve.apply(dfd, resp);
 					}
 				})
 				.fail(function(){
-					rp[j] = (arguments.length < args.length) ? arguments[0] : arguments;
+					resp[j] = (arguments.length < args.length) ? arguments[0] : arguments;
 					if(++ done == args.length){
-						dfd.reject.apply(dfd, rp);
+						dfd.reject.apply(dfd, resp);
 					}
 				})
 			})(i)
